01cb0b3f8ecfed695fbe165a49cf8eabfad31a9e
KevTiv <tivertc.kevin@outlook.com>
2021-07-09 13:46:50 +0200

    youtube upper part is react component is done. CSS needs to be applied to be more visually correct.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UpperTopBar from './top_bar/upper_top_bar/UpperTopBar'
+
+function App() {
+  return (
+    <div className="App">
+          <UpperTopBar />
+    </div>
+  );
+}
+
+export default App;
